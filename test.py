7b632fc606dd5b184d4088bf3a45bc8177d0af90
Sanjeev Tyagi <34897955+sanjeevtyagi170@users.noreply.github.com>
2023-07-03 08:46:31 +0000

    Keras linkedin files added

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
 import pandas as pd
 import numpy as np
-x=10
-print(f'value of x is {x}')
+
+# import keras
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+# !pip install tensorflow
+
+training_data=datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
+test_data=datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
