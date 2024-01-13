4c21487c0ccf52deff2e9d7e9d745eb23429b3fc
Sanjeev Tyagi <34897955+sanjeevtyagi170@users.noreply.github.com>
2024-01-14 01:04:01 +0530

    Update dag.py

--- kafka-airflow-ETL-pipeline/dag.py
+++ kafka-airflow-ETL-pipeline/dag.py
@@ -1,8 +1,9 @@
 # import the libraries
 from datetime import timedelta
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator, PythonOperator
+from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
+
 #defining DAG arguments
 default_args = {
     'owner': 'Sanjeev Tyagi',
@@ -11,7 +12,7 @@ default_args = {
     'email_on_failure': True,
     'email_on_retry': True,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),# retry after 5 minutes
+    'retry_delay': timedelta(minutes=5) # retry after 5 minutes
 }
 
 
@@ -20,35 +21,35 @@ dag = DAG(
     'ETL_toll_data',
     default_args=default_args,
     description='Apache Airflow Final Assignment',
-    schedule_interval=timedelta(minutes=1)
+    schedule_interval='@daily'
+    #schedule_interval=timedelta(minutes=1)
 )
 
 # task unzip data
 unzip_data = BashOperator(
     task_id='unzip_data',
-    bash_command='tar -xzvf /home/project/airflow/dags/finalassignment/staging/tolldata.tgz',
+    bash_command='tar -xzvf /home/project/airflow/dags/finalassignment/tolldata.tgz -C /home/project/airflow/dags/finalassignment/staging/',
     dag=dag
 )
 
 # task extract csv data
 extract_data_from_csv= BashOperator(
-    task_id='extract_data_from_csv'
-    bash_command='cut -d ',' -f 1,2,3,4 /home/project/airflow/dags/finalassignment/staging/vehicle-data.csv\
-     > /home/project/airflow/dags/finalassignment/staging/csv_data.csv'
-     dag=dag
-     )
+    task_id='extract_data_from_csv',
+    bash_command="cut -d ',' -f 1,2,3,4 /home/project/airflow/dags/finalassignment/staging/vehicle-data.csv > /home/project/airflow/dags/finalassignment/staging/csv_data.csv",
+    dag=dag
+    )
 
 # task extract tsv data
 extract_data_from_tsv= BashOperator(
-    task_id='extract_data_from_tsv'
-    bash_command='cut -f 5,6,7 /home/project/airflow/dags/finalassignment/staging/tollplaza-data.tsv > /home/project/airflow/dags/finalassignment/staging/tsv_data.csv'
+    task_id='extract_data_from_tsv',
+    bash_command='cut -f 5,6,7 /home/project/airflow/dags/finalassignment/staging/tollplaza-data.tsv > /home/project/airflow/dags/finalassignment/staging/tsv_data.csv',
     dag=dag
 )
 
 # task extract text data
-extract_data_from_fixed_width= BashOperator(
-    task_id=' extract_data_from_fixed_width'
-    bash_command='cut -c 58-62,63-71 /home/project/airflow/dags/finalassignment/staging/payment-data.txt > /home/project/airflow/dags/finalassignment/staging/fixed_width_data.csv'
+extract_data_from_fixed_width = BashOperator(
+    task_id='extract_data_from_fixed_width',
+    bash_command='cut -c 58-62,63-71 /home/project/airflow/dags/finalassignment/staging/payment-data.txt > /home/project/airflow/dags/finalassignment/staging/fixed_width_data.csv',
     dag=dag
 )
 
@@ -62,5 +63,19 @@ consolidate_data = BashOperator(
     dag=dag
 )
 
+# task delete unnecessary files
+delete_files = BashOperator(
+    task_id='delete_files',
+    bash_command='rm /home/project/airflow/dags/finalassignment/staging/csv_data.csv \
+    /home/project/airflow/dags/finalassignment/staging/tsv_data.csv \
+    /home/project/airflow/dags/finalassignment/staging/fixed_width_data.csv \
+    /home/project/airflow/dags/finalassignment/staging/vehicle-data.csv \
+    /home/project/airflow/dags/finalassignment/staging/tollplaza-data.tsv \
+    /home/project/airflow/dags/finalassignment/staging/payment-data.txt \
+    /home/project/airflow/dags/finalassignment/staging/extracted_data.csv \
+    /home/project/airflow/dags/finalassignment/staging/fileformats.txt',
+    dag=dag
+)
 
-unzip_data>>extract_data_from_csv>>extract_data_from_tsv>>extract_data_from_fixed_width>>consolidate_data
+
+unzip_data>>extract_data_from_csv>>extract_data_from_tsv>>extract_data_from_fixed_width>>consolidate_data>>delete_files
