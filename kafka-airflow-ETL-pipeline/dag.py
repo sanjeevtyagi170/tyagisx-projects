7a79f6396084e25f48997b09cc4c3f0093fdb826
Sanjeev Tyagi <34897955+sanjeevtyagi170@users.noreply.github.com>
2024-01-13 14:42:05 +0530

    Update dag.py

--- kafka-airflow-ETL-pipeline/dag.py
+++ kafka-airflow-ETL-pipeline/dag.py
@@ -1,19 +1,24 @@
 # import the libraries
 from datetime import timedelta
-# The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
-# Operators; we need this to write tasks!
-from airflow.operators.bash_operator import BashOperator
-# This makes scheduling easy
+from airflow.operators.bash_operator import BashOperator, PythonOperator
 from airflow.utils.dates import days_ago
 #defining DAG arguments
-# You can override them on a per-task basis during operator initialization
 default_args = {
     'owner': 'Sanjeev Tyagi',
     'start_date': days_ago(0),# start the DAG today
     'email': ['sanjeev.tyagi170@gmail.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
+    'email_on_failure': True,
+    'email_on_retry': True,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=5),# retry after 5 minutes
 }
+
+
+# defining the DAG
+dag = DAG(
+    'ETL_toll_data',
+    default_args=default_args,
+    description='Apache Airflow Final Assignment',
+    schedule_interval=timedelta(minutes=1),
+)
